81cf14823930131e95aef734abdc956286ccc6b4
lixianjing <xianjimli@hotmail.com>
2026-05-26 11:35:29 +0800

    improve build scripts

--- SConstruct
+++ SConstruct
@@ -5,10 +5,12 @@ helper = app.Helper(ARGUMENTS)
 helper.add_libs(['modbus'])
 helper.set_dll_def('src/modbus.def').call(DefaultEnvironment)
 
-SConsFiles = [
-    'src/SConscript',
-    'demos/SConscript',
-    'tests/SConscript',
-]
+SConsFiles = ['src/SConscript']
+
+if helper.get_curr_config().get_value('BUILD_DEMOS', True) :
+  SConsFiles += ['demos/SConscript']
+
+if helper.get_curr_config().get_value('BUILD_TESTS', True) :
+  SConsFiles += ['tests/SConscript']
 
 SConscript(SConsFiles)
--- scripts/app_helper.py
+++ scripts/app_helper.py
@@ -4,4 +4,9 @@ def Helper(ARGUMENTS):
     locator.init(ARGUMENTS)
 
     from app_helper_base import AppHelperBase
+    from app_helper_base import COMPILE_CONFIG
+    
+    COMPILE_CONFIG['BUILD_TESTS'] = { 'value' : True, 'type' : bool.__name__, 'desc' : ['build awtk-modbus\'s gtest demo'], 'help_info' : 'build awtk-modbus\'s gtest demo, value is true or false, default value is true' }
+    COMPILE_CONFIG['BUILD_DEMOS'] = { 'value' : True, 'type' : bool.__name__, 'desc' : ['build awtk-modbus\'s demo examples'], 'help_info' : 'build awtk-modbus\'s demo examples, value is true or false, default value is true' }
+
     return AppHelperBase(ARGUMENTS)
